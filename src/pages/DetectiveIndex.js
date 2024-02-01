338ef9e6d7600ee3cbd59f8db128f0c1acf725a3
Megan DeRisi <Megan@Lauras-MacBook-Pro.local>
2024-02-01 14:33:46 -0600

    styling to index

--- src/pages/DetectiveIndex.js
+++ src/pages/DetectiveIndex.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import { Card, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
+import './DetectiveIndex.scss'
 
 const DetectiveIndex = ({ detectives }) => {
    return (
-      <main>
+      <main className='main'>
          <h1>SVU Tinder</h1>
          {detectives.map((detective, index) => {
             return (
+               <div className='card-body'>
                <Card
                   style={{
                   width: "14rem"
                 }}
             key={index}
             >
-        <img alt={`profile of a detective named ${detective.name}`} src={detective.image} />
+        <img alt={`profile of a detective named ${detective.name}`} className='image' src={detective.image} />
         <CardBody>
-          <CardTitle tag="h5">{detective.name}</CardTitle>
-          <CardSubtitle className="mb-2 text-muted" tag="h6">
+          <CardTitle className='title' tag="h5">{detective.name}</CardTitle>
+          <CardSubtitle className="mb-2 text-muted subtitle" tag="h6">
             Age: {detective.age}
           </CardSubtitle>
-          <NavLink to={`/detectiveshow/${detective.id}`} className="nav-link">
+          <NavLink to={`/detectiveshow/${detective.id}`} className="button nav-link">
             See More Details
           </NavLink>
         </CardBody>
       </Card>
+      </div>
             )
          })}
       </main>
