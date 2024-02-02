8c38d993335b2762b5a12bd8593dce94c2c38e23
Megan DeRisi <Megan@Lauras-MacBook-Pro.local>
2024-02-02 15:38:48 -0600

    added more styling

--- src/pages/DetectiveShow.js
+++ src/pages/DetectiveShow.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
-import { Button } from 'reactstrap'
+import { Button, Card, CardBody, CardColumns, Row } from 'reactstrap'
+import './DetectiveShow.scss'
 
 const DetectiveShow = ({ detectives, deleteDetective }) => {
    const { id } = useParams()
@@ -9,21 +10,37 @@ const DetectiveShow = ({ detectives, deleteDetective }) => {
    let selectedDetective = detectives.find(detective => detective.id === +id)
 
    return (
-      <div>
-         <img src={selectedDetective ? selectedDetective.image : 'Detective not found'} alt="Detective" />
-         <h1>{selectedDetective ? selectedDetective.name : 'Detective not found'}</h1>
-         <p>{selectedDetective ? selectedDetective.age : 'Detective not found'}</p>
-         <p>{selectedDetective ? selectedDetective.hobbies : 'Detective not found'}</p>
-         <p>{selectedDetective ? selectedDetective.interested : 'Detective not found'}</p>
-         <p>{selectedDetective ? selectedDetective.pronouns : 'Detective not found'}</p>
-         <p>{selectedDetective ? selectedDetective.quotes : 'Detective not found'}</p>
-         <p>{selectedDetective ? selectedDetective.cases : 'Detective not found'}</p>
-         <NavLink to={`/detectiveedit/${selectedDetective ? selectedDetective.id : ''}`} className="nav-link">
-            Edit Detective Profile
-         </NavLink>
-         <NavLink to="/detectiveindex">
-            <Button onClick={() => deleteDetective(id)}>Delete Detective Profile</Button>
-         </NavLink>
+      <div className='show-body'>
+      <div >
+         <Row>
+         <CardColumns style={{width: '37rem'}}>
+            <Card className="show-card">
+            <CardBody>
+               <img className="card-image" src={selectedDetective ? selectedDetective.image : 'Detective not found'} alt="Detective" />
+                  <h1>{selectedDetective ? selectedDetective.name : 'Detective not found'}</h1>
+                  <div className="show-card-text">
+                     <p>Age: {selectedDetective ? selectedDetective.age : 'Detective not found'}</p>
+                     <p>Hobbies: {selectedDetective ? selectedDetective.hobbies : 'Detective not found'}</p>
+                     <p>Preference: {selectedDetective ? selectedDetective.interested : 'Detective not found'}</p>
+                     <p>Pronouns: {selectedDetective ? selectedDetective.pronouns : 'Detective not found'}</p>
+                     <p>Quotes: {selectedDetective ? selectedDetective.quotes : 'Detective not found'}</p>
+                     <p>Favorite Cases: {selectedDetective ? selectedDetective.cases : 'Detective not found'}</p>
+                  </div>
+               <div className="horizonatal-buttons d-flex">
+               <NavLink to={`/detectiveedit/${selectedDetective ? selectedDetective.id : ''}`} className="nav-link">
+                  <Button className='show-button'>
+                  Edit Detective Profile
+                  </Button>
+               </NavLink>
+               <NavLink to="/detectiveindex">
+                  <Button className='show-button' onClick={() => deleteDetective(id)}>Delete Detective Profile</Button>
+               </NavLink>
+               </div>
+               </CardBody>
+            </Card>
+            </CardColumns>
+         </Row>
+      </div>
       </div>
    )
 }
